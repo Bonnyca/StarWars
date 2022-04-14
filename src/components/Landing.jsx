3de858a9b7477d1cc4036cb52b2ed766da073859
Bonny <bonnycharade@gmail.com>
2022-04-14 12:26:39 -0700

    Draft of the landing page.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,32 +1,29 @@
-
+import "./Landing.css";
 const Landing = () => {
-    return (
-        <div className='landing'>
-            <header>
-                <h1>
-                    May the Force be with you.
-                </h1>
-                </header>
-                <div className='movie-list'>
-                    <div className='movie'>
-                        <h1> Movie 1</h1>
-                        <p> Slogan1</p>
-                    </div>
-                    <div className='movie'>
-                        <h1> Movie 1</h1>
-                        <p> Slogan1</p>
-                    </div>
-                    <div className='movie'>
-                        <h1> Movie 1</h1>
-                        <p> Slogan1</p>
-                    </div>
+  return (
+    <div className="landing">
+      <header>
+        <h1>May the Force be with you.</h1>
+        <p className="credentials">Photo by María Ten, Unsplash</p>
+      </header>
 
-                </div>
-
-            
-
+      <div className="movie-wrapper">
+        <div className="movie-list">
+          <div className="movie">
+            <h2> Movie 1</h2>
+            <p> Slogan1</p>
+          </div>
+          <div className="movie">
+            <h2> Movie 1</h2>
+            <p> Slogan1</p>
+          </div>
+          <div className="movie">
+            <h2> Movie 1</h2>
+            <p> Slogan1</p>
+          </div>
         </div>
-    )
-
-}
+      </div>
+    </div>
+  );
+};
 export default Landing;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,10 @@
+import "./MovieList.css";
+
+const MovieList = () => {
+  return (
+    <div className="movie-list">
+     movies
+    </div>
+  );
+};
+export default MovieList;
