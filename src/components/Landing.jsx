09f9b716cdc424f4627475ab319b794ac6a6d28f
Bonny <bonnycharade@gmail.com>
2022-04-13 22:35:24 -0700

    Reset CSS

---
+++ src/components/Landing.jsx
@@ -0,0 +1,32 @@
+
+const Landing = () => {
+    return (
+        <div className='landing'>
+            <header>
+                <h1>
+                    May the Force be with you.
+                </h1>
+                </header>
+                <div className='movie-list'>
+                    <div className='movie'>
+                        <h1> Movie 1</h1>
+                        <p> Slogan1</p>
+                    </div>
+                    <div className='movie'>
+                        <h1> Movie 1</h1>
+                        <p> Slogan1</p>
+                    </div>
+                    <div className='movie'>
+                        <h1> Movie 1</h1>
+                        <p> Slogan1</p>
+                    </div>
+
+                </div>
+
+            
+
+        </div>
+    )
+
+}
+export default Landing;
