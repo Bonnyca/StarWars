102559c191d38759617450f96f411c13420c4f13
Bonny <bonnycharade@gmail.com>
2022-04-17 08:38:11 -0700

    Get movie titles using API request.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,30 @@
-import { NavLink } from 'react-router-dom';
-import st from './MovieList.module.css';
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import st from "./MovieList.module.css";
+import { BASE_URL } from "../../constants";
 
 const MovieList = (props) => {
-
+  const [movies, setMovies] = useState([]);
+  const url = "api/films";
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    fetch(`${BASE_URL}/${url}`)
+      .then((res) => res.json())
+      .then(
+        (res) => setMovies(res.results),
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
 
   return (
     <div className={st.movie_list}>
-        <NavLink to="/movies/2">movie title2</NavLink>
-        <NavLink to="/movies/3">movie title3</NavLink>
-        <NavLink to="/movies/1">movie title1</NavLink>
-
+      {movies.map((movie) => (
+        <NavLink key={movie.episode_id} to={`/movies/${movie.episode_id}`}>
+          {movie.title}
+        </NavLink>
+      ))}
     </div>
   );
 };
