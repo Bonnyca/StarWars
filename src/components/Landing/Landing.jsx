71b916871e782d9a2c439a07686bc58ef8040390
Bonny <bonnycharade@gmail.com>
2022-04-17 22:45:26 -0700

    Improve style of Movie component. Import Font Awesome libraries.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,4 @@
 import st from "./Landing.module.css";
-// import Sword from "./Sword/Sword";
 import { NavLink } from "react-router-dom";
 import MovieList from "../MovieList/MovieList";
 
@@ -10,7 +9,7 @@ const Landing = () => {
         <h1>May the Force be with you.</h1>
         <p className={st.credentials}>Photo by María Ten, Unsplash</p>
       </header>
-      
+
       <div className={st.movie_wrapper}>
         <MovieList/>
       </div>
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,43 +1,72 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import st from "./Movie.module.css";
 import { BASE_URL } from "./../../constants";
-import MovieList from "../MovieList/MovieList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faJedi } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGalacticRepublic,
+  faGalacticSenate,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
+  const [isLoading, setIsloading] = useState(true);
   let { id } = useParams();
   const url = `api/films/${id}`;
-  console.log('mm')
   const fetchMovie = async () => {
     const apiCall = await fetch(`${BASE_URL}/${url}`);
     const movie = await apiCall.json();
     setMovie(movie);
+    setIsloading(false);
   };
 
   useEffect(() => {
     fetchMovie();
   }, [id]);
-
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className={st.movie_item}>
       <div className={st.list}>Movie List</div>
+      {/* TODO Movie list */}
       <div className={st.content}>
         <h1>{movie.title}</h1>
-        <div> <span>Episode ID: </span> {movie.episode_id} </div>
-        <div> <span>Director: </span> {movie.director} </div>
-        <div><span>Producer: </span> {movie.producer} </div>
-        <div><span>Release year:  </span> {movie.release_date} </div> 
-        {/* TODO slice(0,4) */}
+        <div>
+          {" "}
+          <span>Episode ID: </span> {movie.episode_id}{" "}
+        </div>
+        <div>
+          {" "}
+          <span>Director: </span> {movie.director}{" "}
+        </div>
+        <div>
+          <span>Producer: </span> {movie.producer}{" "}
+        </div>
+        <div>
+          <span>Release year: </span> {movie.release_date.slice(0, 4)}{" "}
+        </div>
         <div className={st.statistic}>
-          <div>Characters</div>
-          <div>Starships</div>
-          <div>Planet visited</div>
+          <div className={st.stat_item}>
+            <FontAwesomeIcon icon={faJedi} size="5x" />
+            <p>Characters</p>
+          </div>
+          <div className={st.stat_item}>
+            <FontAwesomeIcon icon={faGalacticSenate} size="5x"/>
+
+            <p>Starships</p>
+          </div>
+          <div className={st.stat_item}>
+            <FontAwesomeIcon icon={faGalacticRepublic} size="5x" />
+            <p>Planets</p>
+          </div>
         </div>
       </div>
       <div className={st.crawl}>
         <p> {movie.opening_crawl} </p>
-        </div>
+      </div>
     </div>
   );
 };
