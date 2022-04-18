042cfe56ca2e710d0fb8464d24d7b6dc63881f46
Bonny <bonnycharade@gmail.com>
2022-04-17 20:55:40 -0700

    Improve layout and style of Movie component.

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,30 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import st from './Movie.module.css';
-import { BASE_URL} from './../../constants'
+import st from "./Movie.module.css";
+import { BASE_URL } from "./../../constants";
+import MovieList from "../MovieList/MovieList";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
   let { id } = useParams();
   const url = `api/films/${id}`;
-
+  console.log('mm')
   const fetchMovie = async () => {
     const apiCall = await fetch(`${BASE_URL}/${url}`);
     const movie = await apiCall.json();
     setMovie(movie);
-  }
-  console.log(movie)
+  };
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchMovie();
-  }, [id]
-  )
+  }, [id]);
 
   return (
     <div className={st.movie_item}>
-      <h1> {id} + 'test' </h1>
-      <h1>{movie.title}</h1>
-      <p>{movie.director}</p>
+      <div className={st.list}>Movie List</div>
+      <div className={st.content}>
+        <h1>{movie.title}</h1>
+        <div> <span>Episode ID: </span> {movie.episode_id} </div>
+        <div> <span>Director: </span> {movie.director} </div>
+        <div><span>Producer: </span> {movie.producer} </div>
+        <div><span>Release year:  </span> {movie.release_date} </div> 
+        {/* TODO slice(0,4) */}
+        <div className={st.statistic}>
+          <div>Characters</div>
+          <div>Starships</div>
+          <div>Planet visited</div>
+        </div>
+      </div>
+      <div className={st.crawl}>
+        <p> {movie.opening_crawl} </p>
+        </div>
     </div>
   );
 };
