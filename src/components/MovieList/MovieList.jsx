d76da2ffb2b96838978a210d8959c530df7c69d7
Bonny <bonnycharade@gmail.com>
2022-04-24 12:57:29 -0700

    Refactor class-based component MovieList to function-based.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,63 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { BASE_URL } from "../../constants";
+import swapiModule from "../../services/swapi";
 import st from "./MovieList.module.css";
 
 const convertDate = (date) => {
-  /* for UI needs */
+  /* Displays date in local US format. */
   let d = new Date(date);
   let options = { year: "numeric", month: "long", day: "numeric" };
   return d.toLocaleString("us-US", options);
 };
 
-class MovieList extends React.Component {
-  /* Class component fetches data on ComponentDidMount() and renders it. 
-  TO-DO: split on Higher-Order Component and displey components and reuse in sidebar.*/
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: [],
-      isLoading: true,
-    };
+const MovieList = () => {
+  const [movies, setMovies] = useState({});
+  const [isLoading, setIsLoadng] = useState(true);
+
+  const fetchMovies = () => {
+    /* A func to get a list of the films and set them to the state. */
+    swapiModule.getFilms(function (data) {
+      setMovies(data.results);
+      setIsLoadng(false);
+    });
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
-  componentDidMount() {
-    const url = "api/films";
+  return (
+    <div className={st.movie_list}>
+      {movies.map((movie) => (
+        <div key={movie.episode_id} className={st.item}>
+          <NavLink
+            to={`/movies/${movie.url.replace(/\/$/, "").split("/").pop()}`}
+          >
+            <p className={st.date}>{convertDate(movie.release_date)}</p>
+            <p className={st.title}>{movie.title}</p>
+          </NavLink>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-    fetch(`${BASE_URL}/${url}`)
-      .then((res) => res.json())
-      .then(
-        (res) =>
-          this.setState({
-            movies: res.results,
-            isLoading: false,
-          }),
-        (error) => {
-          console.log(error);
-        }
-      );
-  }
-
-  render() {
-    const { movies, isLoading } = this.state;
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-
-    return (
-      <div className={st.movie_list}>
-        {movies.map((movie) => (
-          <div key={movie.episode_id} className={st.item}>
-            <NavLink
-              to={`/movies/${movie.url.replace(/\/$/, "").split("/").pop()}`}
-            >
-              <p className={st.date}>{convertDate(movie.release_date)}</p>
-              <p className={st.title}>{movie.title}</p>
-            </NavLink>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
 export default MovieList;
